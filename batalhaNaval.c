6f22d396ff9a62c6f856503553a07c25b3cb20ac
João <joaolima1802@gmail.com>
2025-09-14 09:06:49 +0000

    Desafio intermediário concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,8 +6,12 @@
 
 // LEGENDA: 0 - Água | 3 - Navio
 
+// Definindo uma constante para o tamanho do tabuleiro
+#define LIN 10
+#define COL 10
+
 // Código para a exibição do tabuleiro
-void exibirTabuleiro(int tabuleiro[10][10]){
+void exibirTabuleiro(int tabuleiro[LIN][COL]){
     int linha, coluna;
 
         printf("\t");
@@ -27,7 +31,7 @@ void exibirTabuleiro(int tabuleiro[10][10]){
 
 int main() {
     // Inicialização do tabuleiro
-    int tabuleiro[10][10] = {
+    int tabuleiro[LIN][COL] = {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -42,11 +46,17 @@ int main() {
     // Inicialização dos navios e definição de suas coordenadas
     int navioUm[3] = {3, 3, 3};
     int navioDois[3] = {3, 3, 3};
+    int navioTres[3] = {3, 3, 3};
+    int navioQuatro[3] = {3, 3, 3};
 
     int linhaNavioUm = 0;
     int colunaNavioUm = 3;
     int linhaNavioDois = 6;
     int colunaNavioDois = 7;
+    int linhaNavioTres = 1;
+    int colunaNavioTres = 1;
+    int linhaNavioQuatro = 7;
+    int colunaNavioQuatro = 3;
 
     // Variáveis auxiliares
     int linha, coluna, i;
@@ -102,7 +112,64 @@ int main() {
     printf("O navio não cabe no tabuleiro!\n\n");
     }
 
+    // Posicionamento do terceiro navio
+    printf("E um navio na diagonal, assim eles não nos verão chegando!\n");
+
+    if (linhaNavioTres + 3 <= 10){ // verificação se o navio cabe no tabuleiro
+        int semSobreposicao = 1; // flag de verificação de sobreposição
+
+        for (i = 0; i < 3; i++){
+            if (tabuleiro[linhaNavioTres + i][colunaNavioTres + i] != 0){
+                semSobreposicao = 0;
+                break;
+            }
+        }
+        if (semSobreposicao == 1) {
+            for (i = 0; i < 3; i++){
+            tabuleiro[linhaNavioTres + i][colunaNavioTres + i] = navioTres[i];
+            }
+        printf("Navio posicionado! Suas coordenadas são: ");
+        for (i = 0; i < 3; i++) { // exibição de coordenadas
+            printf("%c%d ", (char)(('A' + colunaNavioTres) + i), (linhaNavioTres + i) + 1);
+        }
+        printf("\n\n");
+        } else {
+        printf("Impossível fazer isso, as coordenadas já estão ocupadas!\n\n");
+        }
+    } else {
+    printf("O navio não cabe no tabuleiro!\n\n");
+    }
+
+    // Posicionamento do quarto navio
+    printf("MAIS! Mais um na diagonal! Precisamos garantir vantagem!\n");
+
+    if (linhaNavioQuatro + 3 <= 10){ // verificação se o navio cabe no tabuleiro
+        int semSobreposicao = 1; // flag de verificação de sobreposição
+
+        for (i = 0; i < 3; i++){
+            if (tabuleiro[linhaNavioQuatro + i][colunaNavioQuatro - i] != 0){
+                semSobreposicao = 0;
+                break;
+            }
+        }
+        if (semSobreposicao == 1) {
+            for (i = 0; i < 3; i++){
+            tabuleiro[linhaNavioQuatro + i][colunaNavioQuatro - i] = navioQuatro[i];
+            }
+        printf("Navio posicionado! Suas coordenadas são: ");
+        for (i = 0; i < 3; i++) { // exibição de coordenadas
+            printf("%c%d ", (char)(('A' + colunaNavioQuatro) - i), (linhaNavioQuatro + i) + 1);
+        }
+        printf("\n\n");
+        } else {
+        printf("Impossível fazer isso, as coordenadas já estão ocupadas!\n\n");
+        }
+    } else {
+    printf("O navio não cabe no tabuleiro!\n\n");
+    }
+
     // Exibição do tabuleiro com os navios posicionados (representados por 3)
+    printf("Muito bem, marinheiro! Vamos ver como ficaram nossas belezuras em alto mar!\n\n");
     exibirTabuleiro(tabuleiro);
 
     return 0;
